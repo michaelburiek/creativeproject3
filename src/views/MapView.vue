a7f422552c7d1402199027ecd644e46e42847823
michaelburiek <michaelburiek0@gmail.com>
2022-03-12 16:18:34 -0800

    Set-up Pokedex & Map comp/view

---
+++ src/views/MapView.vue
@@ -0,0 +1,22 @@
+<template>
+    <PokemonMap />
+</template>
+
+<script>
+// @ is an alias to /src
+import PokemonMap from '@/components/PokemonMap.vue' //Import the component
+
+export default {
+    name: 'MapView',
+    components: { PokemonMap }, //Register the component
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/PokedexView.vue
@@ -0,0 +1,22 @@
+<template>
+    <PokedexAPI />
+</template>
+
+<script>
+// @ is an alias to /src
+import PokedexAPI from '@/components/PokedexAPI.vue' //Import the component
+
+export default {
+    name: 'PokedexView',
+    components: { PokedexAPI }, //Register the component
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
