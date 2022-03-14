3626d0f1c38a80f2195293affcf203af40b51c62
michaelburiek <michaelburiek0@gmail.com>
2022-03-13 19:02:34 -0700

    Type buttons & filter/display methods

--- src/components/PokedexAPI.vue
+++ src/components/PokedexAPI.vue
@@ -6,6 +6,21 @@
         <button @click="fetchPokemon('fighting')" class="button fighting">FIGHTING</button>
         <button @click="fetchPokemon('flying')" class="button flying">FLYING</button>
         <button @click="fetchPokemon('poison')" class="button poison">POISON</button>
+        <button @click="fetchPokemon('ground')" class="button ground">GROUND</button>
+        <button @click="fetchPokemon('rock')" class="button rock">ROCK</button>
+        <button @click="fetchPokemon('bug')" class="button bug">BUG</button>
+        <button @click="fetchPokemon('ghost')" class="button ghost">GHOST</button>
+        <button @click="fetchPokemon('steel')" class="button steel">STEEL</button>
+        <button @click="fetchPokemon('fire')" class="button fire">FIRE</button>
+        <button @click="fetchPokemon('water')" class="button water">WATER</button>
+        <button @click="fetchPokemon('grass')" class="button grass">GRASS</button>
+        <button @click="fetchPokemon('electric')" class="button electric">ELECTRIC</button>
+        <button @click="fetchPokemon('psychic')" class="button psychic">PSYCHIC</button>
+        <button @click="fetchPokemon('ice')" class="button ice">ICE</button>
+        <button @click="fetchPokemon('dragon')" class="button dragon">DRAGON</button>
+        <button @click="fetchPokemon('dark')" class="button dark">DARK</button>
+        <button @click="fetchPokemon('fairy')" class="button fairy">FAIRY</button>
+
       </div>
 
       <div class="pokedex-container">
@@ -28,19 +43,6 @@ export default {
   },
   methods: {
       fetchPokemon(pokemonType) {
-          
-          //Filters pokemon of specific type
-          const filterPokemon = (pokemon, pokemonType) => {
-              const filteredPokemon = [];
-              console.log(pokemon);
-              console.log(pokemonType);
-              console.log(filteredPokemon);
-              
-              
-
-          };
-          
-        
           //Creates array 'promises' and pushes on to promises the json object for every pokemon
           const promises = [];
           for (let i = 1; i <= 150; i++) {
@@ -49,7 +51,7 @@ export default {
               .then((res) => res.json()));
           }
           
-          //
+          //Uses higher order map function to create a new array 'pokemon' and push a few data objects to it
           Promise.all(promises).then((results) => {
               const pokemon = results.map((result) => ({
                   name: result.name,
@@ -60,6 +62,33 @@ export default {
               }));
               filterPokemon(pokemon, pokemonType)
           });
+
+          //Filters pokemon of specific type
+          const filterPokemon = (pokemons, pokemonType) => {
+              const filteredPokemon = pokemons.filter((pokemon) => pokemon.type == pokemonType);
+              console.log(pokemons);                
+              console.log(pokemonType);
+              console.log(filteredPokemon);
+              displayPokemon(filteredPokemon)
+          };
+
+          //Displays filtered pokemon
+          const displayPokemon = (filteredPokemon) => {
+            const pokemonHTMLString = filteredPokemon
+                .map(
+                (filteredPokeman) => 
+                `
+                    <li class="card">
+                        <img class="card-image" src="${filteredPokeman.backImage}"/>
+                        <h2 class="card-title">${filteredPokeman.id}. ${filteredPokeman.name}</h2>
+                        <p class="card-subtitle">Type: ${filteredPokeman.type}</p>
+                    </li>
+                `
+                )
+                .join('');
+                console.log(pokemonHTMLString);
+                this.pokedex = pokemonHTMLString;
+          };
       },
   },
 }
@@ -127,4 +156,118 @@ export default {
         border-radius: 5px;
     }
     .poison:hover {background-color: #A95CA0;}
+
+    .ground {
+        background-color: white;
+        color: black;
+        border: 2px solid #EECC55;
+        border-radius: 5px;
+    }
+    .ground:hover {background-color: #EECC55;}
+
+    .rock {
+        background-color: white;
+        color: black;
+        border: 2px solid #CCBD72;
+        border-radius: 5px;
+    }
+    .rock:hover {background-color: #CCBD72;}
+
+    .bug {
+        background-color: white;
+        color: black;
+        border: 2px solid #C2D21E;
+        border-radius: 5px;
+    }
+    .bug:hover {background-color: #C2D21E;}
+
+    .ghost {
+        background-color: white;
+        color: black;
+        border: 2px solid #7975D7;
+        border-radius: 5px;
+    }
+    .ghost:hover {background-color: #7975D7;}
+
+    .steel {
+        background-color: white;
+        color: black;
+        border: 2px solid #C4C2DB;
+        border-radius: 5px;
+    }
+    .steel:hover {background-color: #C4C2DB;}
+
+    .fire {
+        background-color: white;
+        color: black;
+        border: 2px solid #FA5643;
+        border-radius: 5px;
+    }
+    .fire:hover {background-color: #FA5643;}
+
+    .water {
+        background-color: white;
+        color: black;
+        border: 2px solid #56ADFF;
+        border-radius: 5px;
+    }
+    .water:hover {background-color: #56ADFF;}
+
+    .grass {
+        background-color: white;
+        color: black;
+        border: 2px solid #8CD750;
+        border-radius: 5px;
+    }
+    .grass:hover {background-color: #8CD750;}
+
+    .electric {
+        background-color: white;
+        color: black;
+        border: 2px solid #FDE139;
+        border-radius: 5px;
+    }
+    .electric:hover {background-color: #FDE139;}
+
+    .psychic {
+        background-color: white;
+        color: black;
+        border: 2px solid #FA65B4;
+        border-radius: 5px;
+    }
+    .psychic:hover {background-color: #FA65B4;}
+
+    .ice {
+        background-color: white;
+        color: black;
+        border: 2px solid #96F1FF;
+        border-radius: 5px;
+    }
+    .ice:hover {background-color: #96F1FF;}
+
+    .dragon {
+        background-color: white;
+        color: black;
+        border: 2px solid #8673FF;
+        border-radius: 5px;
+    }
+    .dragon:hover {background-color: #8673FF;}
+
+    .dark {
+        background-color: white;
+        color: black;
+        border: 2px solid #8D6855;
+        border-radius: 5px;
+    }
+    .dark:hover {background-color: #8D6855;}
+
+    .fairy {
+        background-color: white;
+        color: black;
+        border: 2px solid #F9AEFF;
+        border-radius: 5px;
+    }
+    .fairy:hover {background-color: #F9AEFF;}
+
+
 </style>
